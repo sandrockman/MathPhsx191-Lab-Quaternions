accfc2f1e1c9746af0eac3262009265c9fec5111
unknown <svc-student@RC37803.student.directory.jccc>
2016-05-12 18:33:24 -0500

    update work.

---
+++ vhaskin1-Lab-Quaternions/vhaskin1-Lab-Quaternions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vhaskin1_Lab_Quaternions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ vhaskin1-Lab-Quaternions/vhaskin1-Lab-Quaternions/Quaternion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Quaternion
+{
+    public double a { get; set; }
+    public Vector3D vector;
+
+    public Quaternion()
+    {
+        a = 1;
+        vector = new Vector3D();
+    }
+
+    public Quaternion(Vector3D point)
+    {
+        a = 0;
+        vector = new Vector3D(point.XValue, point.YValue, point.ZValue, point.WValue);
+    }
+
+    public Quaternion(double newA, Vector3D newVector)    {
+        a = newA;
+        vector = new Vector3D(newVector.XValue, 
+                              newVector.YValue, 
+                              newVector.ZValue, 
+                              newVector.WValue);
+    }
+}
